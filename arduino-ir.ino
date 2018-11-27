e40c1013f4f534ce464ccd5550c7dcfab8251253
TheLupp <36422511+TheLupp@users.noreply.github.com>
2018-11-27 16:18:22 +0100

    Added waiting for serial and faster loop

--- arduino-ir.ino
+++ arduino-ir.ino
@@ -4,10 +4,16 @@ int serIn; //var that will hold the bytes in read from the serialBuffer
 
 void setup() {
   Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
 }
 
 void loop() {
   // Format for python
-  Serial.println(String(prng.getRndByte()) + ":" + String(prng.getRndByte()));
-  delay(1000);
+  while (true) {
+    Serial.println(String(prng.getRndByte()) + ":" + String(prng.getRndByte()));
+    delay(1000);
+  }
+
 }
