deb5c3ccb4eeaf1195ac36c6a4d5a193db0c6d9e
TheLupp <ondrej.sloup@aol.com>
2018-11-29 22:30:24 +0000

    Complete rework of arduino comunication. Now its not using coreFunctions and portUtils.py, but it uses pyduino.py instead. Unnecessary files moved to backup.

--- index.py
+++ index.py
@@ -1,14 +1,16 @@
 # index.py
-# Main application for python web server 
+# Main application for python web server
 # Features - showing data collected from arduino
 # MIT License
 
 from flask import (
-    Flask, make_response, Blueprint, flash, g, redirect, render_template, request, url_for
+    Flask, make_response, redirect, render_template, request, url_for
 )
+from pyduino import *
+import time
+
 application = Flask(__name__)
 
-#import thread
 import traceback
 import os
 import sys
@@ -17,19 +19,22 @@ CURRENTDIR = os.path.dirname(os.path.abspath(__file__))
 if CURRENTDIR not in sys.path:
     sys.path.insert(0, CURRENTDIR)
 
-from coreFunctions import DisplayCore
+# Initialize connection to Arduino
+a = Arduino()
+time.sleep(3)
+
+# Pins declaration
+LED_PIN = 13
+ANALOG_PIN = 0
+
+a.set_pin_mode(LED_PIN,'O')
+print 'Arduino initialized'
 
 def displayErrorHTML(error):
     # Formats an error in HTML for debugging on web page
     err = "<p>PYTHON ERROR</p>"
     err += "<pre>" + error + "</pre>"
     return err
-
-def backgroundLoop():
-    core = DisplayCore()
-    while True:
-        core.getData()
-
 
 @application.errorhandler(500)
 def internalServerError(error):
@@ -38,33 +43,53 @@ def internalServerError(error):
     err += "<pre>"+ str(traceback.format_exc()) + "</pre>"
     return err
 
-# Renders a simple html page with the data
-@application.route('/', methods=['GET'])
+# Renders main page
+@application.route('/', methods = ['POST','GET'])
 def index():
-    try:
-	# The simple display core
-        core = DisplayCore()
-	core.getData()
-        # Output the HTML
-        response = make_response(core.webUpdate())
-        return response
-    except:
-        # Error
-        response = make_response(displayErrorHTML(traceback.format_exc()))
-        response.headers['Content-Type'] = 'text/html'
-        return response
+    if request.method == 'POST':
+
+        if request.form['submit'] == 'Turn On':
+           print 'TURN ON'
+           a.digital_write(LED_PIN,1)
+
+        elif request.form['submit'] == 'Turn Off':
+            print 'TURN ON'
+            a.digital_write(LED_PIN,0)
+
+        else:
+            pass
+
+    # Read ANALOG value from PIN
+    analogData = a.analog_read(ANALOG_PIN)
+
+    # Renders the final template with given variables
+    return render_template('index.html', temp=100*(analogData/1023.))
 
 # Provides the data in JSON format to do some better things with
+"""
 @application.route('/api/', methods=['GET'])
 def json():
-    core = DisplayCore()
-    core.getData()
-    response = make_response(core.retJSON())
+    response = make_response()
     response.headers['Content-Type'] = 'application/json'
     return response
+"""
+
+# API
+@application.route('/turnon', methods=['GET'] )
+def turn_on():
+    # turn on LED on arduino
+    a.digital_write(LED_PIN,1)
+    return redirect( url_for('index') )
+
+@application.route('/turnoff', methods=['GET'] )
+def turn_off():
+    # turn off LED on arduino
+    a.digital_write(LED_PIN,0)
+    return redirect( url_for('index') )
+
 
 # Run the app
 if __name__ == "__main__":
    # thread.start_new_thread(backgroundLoop());
     application.debug = True
-    application.run()
+    application.run('0.0.0.0')
