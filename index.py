a6392100db1e5b5c3f308b82cf204ec2c7b0dca8
TheLupp <ondrej.sloup@aol.com>
2018-12-12 18:34:50 +0000

    Minor code improvements; Add generator for secretkeys; Better design of buttons; Edited If for executing forms; Add regex for input

--- index.py
+++ index.py
@@ -3,25 +3,25 @@
 # Features - showing data collected from arduino
 # MIT License
 
-from flask_socketio import SocketIO, emit
+import traceback
+import os
+import sys
+
 from threading import Thread, Event
 
 from flask import (
     Flask, make_response, redirect, render_template, request, url_for
 )
+from flask_socketio import SocketIO, emit
+
 from pyduino import *
-import time
 from time import sleep
 
 application = Flask(__name__)
 
-application.config['SECRET_KEY'] = 'secret!'
+application.config['SECRET_KEY'] = os.urandom(28)
 application.config['DEBUG'] = True
 socketio = SocketIO(application)
-
-import traceback
-import os
-import sys
 
 thread = Thread()
 thread_stop_event = Event()
@@ -32,7 +32,7 @@ if CURRENTDIR not in sys.path:
 
 # Initialize connection to Arduino
 a = Arduino()
-time.sleep(3)
+sleep(3)
 
 # Pins declaration
 LED_PIN = 13
@@ -40,7 +40,6 @@ ANALOG_PIN = 0
 
 a.set_pin_mode(LED_PIN,'O')
 print 'Arduino initialized'
-
 
 def displayErrorHTML(error):
     # Formats an error in HTML for debugging on web page
@@ -64,8 +63,8 @@ class ReadAnalogValues(Thread):
         # Reads values from arduino
         print("Reading values")
         while not thread_stop_event.isSet():
-	    number = a.analog_read(ANALOG_PIN)
-            socketio.emit('newnumber', {'number': number}, namespace='/test')
+	    currTemp = a.analog_read(ANALOG_PIN)
+            socketio.emit('newnumber', {'number': currTemp}, namespace='/test')
             sleep(self.delay)
 
     def run(self):
@@ -74,23 +73,32 @@ class ReadAnalogValues(Thread):
 # Renders main page
 @application.route('/', methods = ['POST','GET'])
 def index():
+    number = ''
     if request.method == 'POST':
 
-        if request.form['submit'] == 'Turn On':
+        if 'turnon' in request.form:
            print 'TURN ON'
            a.digital_write(LED_PIN,1)
 
-        elif request.form['submit'] == 'Turn Off':
-            print 'TURN ON'
-            a.digital_write(LED_PIN,0)
+        elif 'turnoff' in request.form:
+           print 'TURN OFF'
+           a.digital_write(LED_PIN,0)
+        elif 'setTemp' in request.form:
+	    print 'Setting temp'
+	    try:
+   		val = request.form['temperature_val']
+		number = float(val)
+   		print('User set temperature to ' + str(number))
+	    except ValueError:
+   		print('User did not wrote a number')
         else:
-            pass
+	    pass
 
     # Read ANALOG value from PIN
-    analogData = 0 #a.analog_read(ANALOG_PIN)
+    #analogData = 0 #a.analog_read(ANALOG_PIN)
 
     # Renders the final template with given variables
-    return render_template('index.html', temp=analogData)
+    return render_template('index.html', temp=number, testVar='Loading...')
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
@@ -125,6 +133,7 @@ def turn_off():
 # Run the app
 if __name__ == "__main__":
     socketio.run(application)
-    application.debug = True
     application.run('0.0.0.0')
+    application.debug = True
+
 
