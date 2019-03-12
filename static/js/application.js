650543645b653b7bc2c7cae5240fa979222d549e
Ondřej Sloup <ondrej.sloup@aol.com>
2019-03-12 15:46:48 +0100

    Fix script

--- static/js/application.js
+++ static/js/application.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
+jQuery(document).ready(function ($) {
     //connect to the socket server.
     var socket = io.connect("http://" + document.domain + ":" + location.port + "/test");
     var numbersReceived = [];
 
     //receive details from server
-    socket.on("newnumber", function(msg) {
-	console.log("Received number" + msg.number);
+    socket.on("newnumber", function (msg) {
+        console.log("Received number" + msg.number);
         //maintain a list of ten numbers
         numbersReceived.push(msg.number);
         $("#test").html(msg.number);
