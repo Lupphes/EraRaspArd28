851fa03805e47f8e842eb88758fb94286585aa89
Heathcliff <patricktsidina@gmail.com>
2019-03-14 08:12:16 +0100

    Implementing Andrew's code

--- arduino-ir.ino
+++ arduino-ir.ino
@@ -124,3 +124,82 @@ void lights(){ // Program that controls lights inside of the house based on the 
           digitalWrite(13, LOW); // Lights off
   }
 }
+
+void set_pin_mode(int pin_number, char mode){
+    /*
+     * Performs a pinMode() operation depending on the value of the parameter
+     * mode :
+     * - I: Sets the mode to INPUT
+     * - O: Sets the mode to OUTPUT
+     * - P: Sets the mode to INPUT_PULLUP
+     */
+
+    switch (mode){
+        case 'I':
+            pinMode(pin_number, INPUT);
+            break;
+        case 'O':
+            pinMode(pin_number, OUTPUT);
+            break;
+        case 'P':
+            pinMode(pin_number, INPUT_PULLUP);
+            break;
+    }
+}
+
+void digital_read(int pin_number){
+    /*
+     * Performs a digital read on pin_number and returns the value read to serial
+     * in this format: D{pin_number}:{value}\n where value can be 0 or 1
+     */
+
+    digital_value = digitalRead(pin_number);
+    Serial.print('D');
+    Serial.print(pin_number);
+    Serial.print(':');
+    Serial.println(digital_value); // Adds a trailing \n
+}
+
+void analog_read(int pin_number){
+    /*
+     * Performs an analog read on pin_number and returns the value read to serial
+     * in this format: A{pin_number}:{value}\n where value ranges from -1023 to 1023
+     */
+
+    float data = analogRead(pin_number); // Reading raw values from sensor
+
+    switch (pin_number)
+    {
+        case 0: // Executes following code if the pin_number equals zero which is room temperature
+            analog_value = (5.0 * data * 100.0) / 1024.0; // Converting raw value to Celsius            
+            break;
+
+        case 1:
+            analog_value = data;
+            break;
+    
+        default:
+            break;
+    }
+
+    Serial.print('A');
+    Serial.print(pin_number);
+    Serial.print(':');
+    Serial.println(analog_value); // Adds a trailing \n
+}
+
+void digital_write(int pin_number, int digital_value){
+    /*
+     * Performs a digital write on pin_number with the digital_value
+     * The value must be 1 or 0
+     */
+  digitalWrite(pin_number, digital_value);
+}
+
+void analog_write(int pin_number, int analog_value){
+    /*
+     * Performs an analog write on pin_number with the analog_value
+     * The value must be range from 0 to 255
+     */
+  analogWrite(pin_number, analog_value);
+}
